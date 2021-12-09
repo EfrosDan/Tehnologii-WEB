0300dcac2d05ffbee1380924919570c1b880010a
EfrosDan <74728462+EfrosDan@users.noreply.github.com>
2021-12-09 10:00:09 +0200

    Add files via upload

--- laborator5-6/contact.php
+++ laborator5-6/contact.php
@@ -1,27 +1,75 @@
-<?php include 'validation.php';?>
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
 <head>
-<title> Contact Form </title>
-<link rel="stylesheet" href="css/contactsyle.css" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="css/contactsyle.css">
+<title>Email form</title>
 </head>
 <body>
-<div class="container">
-<div class="main">
-<h2> Contact Form </h2>
-<form method="post" action="contact.php">
-<input class="input" type="text" placeholder="Name" name="name" value="">
-<span class="error"><?php echo $nameError;?></span>
-<input class="input" type="text" placeholder="Email" name="email" value="">
-<span class="error"><?php echo $emailError;?></span>
-<input class="input" type="text" placeholder="Subject" name="purpose" value="">
-<span class="error"><?php echo $purposeError;?></span>
-<textarea name="message" placeholder="Message" val=""></textarea>
-<span class="error"><?php echo $messageError;?></span>
-<input class="submit" type="submit" name="submit" value="Submit">
-<span class="success"><?php echo $successMessage;?></span>
-</form>
+<div>
+<header>
+		<ul>
+		<li class="act"><a href="homepage.html">Home</a></li>
+		</ul>
+	</header>
 </div>
-</div>
+
+ <div id="contact-form">
+ <center>
+	<div>
+		<h1>Ai o întrebare?</h1> 
+		<h4>Trimite-ne un mesaj.</h4> 
+	</div>
+</center>
+	<form name="contactform" method="post" action="mail.php">
+		<table width="450px">
+		<tr>
+		 <td valign="top">
+		  <label for="first_name">Nume *</label>
+		 </td>
+		 <td valign="top">
+		  <input  type="text" name="first_name" maxlength="50" size="30">
+		 </td>
+		</tr>
+		<tr>
+		<td valign="top">
+		  <label for="last_name">Prenume *</label>
+		</td>
+		 <td valign="top">
+		  <input  type="text" name="last_name" maxlength="50" size="30">
+		 </td>
+		</tr>
+		<tr>
+		 <td valign="top">
+		  <label for="email">Email *</label>
+		 </td>
+		 <td valign="top">
+		  <input  type="text" name="email" maxlength="80" size="30">
+		 </td>
+		</tr>
+		<tr>
+		 <td valign="top">
+		  <label for="telephone">Număr de telefon</label>
+		 </td>
+		 <td valign="top">
+		  <input  type="text" name="telephone" maxlength="30" size="30">
+		 </td>
+		</tr>
+		<tr>
+		 <td valign="top">
+		  <label for="comments">Comentarii *</label>
+		 </td>
+		 <td valign="top">
+		  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+		 </td>
+		</tr>
+		<tr>
+		 <td colspan="2" style="text-align:center">
+		  <input type="submit" value="Trimite">   
+		 </td>
+		</tr>
+		</table>
+		</form>
+	</div>
+
 </body>
 </html>
